12882509f3226645e835880a971afe06261d8779
david manly <manly.david9@gmail.com>
2020-07-13 20:41:47 -0500

    app3git

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      foods:["snickers",
+      "Twix",
+      "Whatchamacallit",
+      "Kit-KAt",
+      "Milky Way",
+      "Nerds Rope"],
+      filterString:""
+    }
+  }
+  handleChange(filter){
+    this.setState({filterString: filter})
+  }
+
+  render(){
+    let displayFoods = this.state.foods.filter((element, index) =>{
+      return element.toLowerCase().includes(this.state.filterString);
+    }).map((element, index) =>{
+      return <h2 key= {index}>{element}</h2>
+    })
+    return (
+      <div className="App">
+        <input onChange={(e) => this.handleChange(e.target.value)} type="text" />
+        {displayFoods}
+      </div>
+    )
+  }
 }
 
 export default App;
